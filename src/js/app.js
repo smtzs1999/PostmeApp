88313adbe4b6b1556a2ad38a41eeccd006a02e2c
smtzs1999 <mtzs19990@gmail.com>
2025-10-27 18:28:06 -0600

    [INT] Frontend Basico de app

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+/* Declaración de variables globales */
+let MAIN;
+let MODAL_POST;
+let BTN_SHOW_POST;
+let BTN_CANCEL_POST;
+
+/* Funciones */
+const showPostModal =()=>{
+  MAIN.style.display= 'none';
+  MODAL_POST.style.display= 'block';
+
+  setTimeout(()=>{
+    MODAL_POST.style.transform = 'translateY(0)';
+  })
+}
+
+const closePostModal =()=>{
+  MAIN.style.display= 'block';
+  MODAL_POST.style.transform='translateY(100vh)';
+}
+
+/* Cuando se cargue el DOM */
+window.addEventListener('load', ()=>{
+  MAIN = document.querySelector('#main');
+  MODAL_POST= document.querySelector('#modal-post-section');
+  BTN_SHOW_POST= document.querySelector('#btn-upload-post');
+  BTN_CANCEL_POST= document.querySelector('#btn-post-cancel');
+
+  /* Agregando eventos a botones */
+  BTN_SHOW_POST.addEventListener('click', showPostModal);
+  BTN_CANCEL_POST.addEventListener('click', closePostModal)
+})
